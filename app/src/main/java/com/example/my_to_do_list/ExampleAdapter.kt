ad77e62a7dc055a97231d4228ce2fa5dcef72953
sbbogdy <72346254+sbbogdy@users.noreply.github.com>
2020-10-11 15:29:03 +0300

    work on 09-10/11/20

---
+++ app/src/main/java/com/example/my_to_do_list/ExampleAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.my_to_do_list
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.example_item.view.*
+
+class ExampleAdapter(private val exampleList: List<ExampleItem>) : RecyclerView.Adapter<ExampleAdapter.ExampleViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExampleViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.example_item,
+        parent, false)
+
+        return ExampleViewHolder(itemView)
+    }
+
+    override fun getItemCount() = exampleList.size
+
+    override fun onBindViewHolder(holder: ExampleViewHolder, position: Int) {
+        val currentItem = exampleList[position]
+
+        holder.imageView.setImageResource(currentItem.imageResource)
+        holder.lstView.text = currentItem.text1
+    }
+
+    class ExampleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.image_view
+        val lstView: TextView = itemView.lstTask
+    }
+}
--- app/src/main/java/com/example/my_to_do_list/MainActivity.kt
+++ app/src/main/java/com/example/my_to_do_list/MainActivity.kt
@@ -10,20 +10,36 @@ package com.example.my_to_do_list
     class MainActivity : AppCompatActivity() {
         val context = this
         var db = DatabaseHandler(context)
+
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_main)
-            var data =db.readData()
-            val lstText : TextView = findViewById(R.id.lstTask) as TextView
-            for (i in 0 .. ( data.size-1)){
-                lstText.append(data.get(i).task)
+
+
+            val exampleList = generateList(500)
+
+            recycler_view.adapter = ExampleAdapter(exampleList)
+            recycler_view.layoutManager = LinearLayoutManager(this)
+            recycler_view.setHasFixedSize(false)
             }
+        private  fun generateList(size : Int): List<ExampleItem>
+        {
+            var data =db.readData()
+            val list = ArrayList<ExampleItem>()
+
+            for ( i in 0 until (data.size) ){
+                val lstText : String = data.get(i).task
+                val item = ExampleItem (R.drawable.ic_android, lstText, null.toString())
+                list +=item
+            }
+            return list
         }
 
         fun addItem(view: View): Unit {
             val intent = Intent(context, AddActivity::class.java)
             startActivity(intent)
         }
+
 
     }
 
